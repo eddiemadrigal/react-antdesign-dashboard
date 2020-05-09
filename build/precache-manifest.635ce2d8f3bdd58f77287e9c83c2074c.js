25559acc0e6954e1521429b2964b65ea9f06457d
Eddie Madrigal <edmadrigal@yahoo.com>
2020-05-08 17:17:14 -0700

    responsive grid component added

---
+++ build/precache-manifest.635ce2d8f3bdd58f77287e9c83c2074c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2783f5e2b79c08b7dd907765d09fa8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88829c1ca3e8568eb18a",
+    "url": "/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "24adc35e3857a0bd92d3",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "88829c1ca3e8568eb18a",
+    "url": "/static/js/2.6d285f6b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6d285f6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24adc35e3857a0bd92d3",
+    "url": "/static/js/main.ca7bd150.chunk.js"
+  },
+  {
+    "revision": "60375d6666529c06de65",
+    "url": "/static/js/runtime-main.1adefc23.js"
+  }
+]);
--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,65 @@
 import React from 'react';
-import { Typography, Layout, Avatar, Menu, Breadcrumb } from 'antd';
+import { Typography, Layout, Avatar, Menu, Breadcrumb, Row, Col } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 import SubMenu from 'antd/lib/menu/SubMenu';
 import './App.css';
 
-const { Header, Footer, Sider, Content } = Layout;
+const { Content } = Layout;
 const { Title } = Typography;
 
 function App() {
 
   return (
     <div className="App">
-      <Layout>
-        <Header style={{ backgroundColor: '#000', color: '#fff', padding: 16}}>
-          <Avatar style={{ float: 'right', marginTop: -5 }} size="large" icon={<UserOutlined />} />
-          <Title level={3} style={{ color: '#fff' }}>Eddie Madrigal</Title>
-        </Header>
-        <Layout>
-          <Sider style={{ backgroundColor: '#ccc' }}>
-            <Menu 
-              defaultSelectedKeys={['dashboard']}
-              mode='inline'
-            >
-              <Menu.Item key='dashboard'>
-                Dashboard
-              </Menu.Item>
-              <SubMenu 
-                title={
-                  <span>Social Media</span>
-                }>
-                <Menu.ItemGroup
-                  key='socialmediagroup' 
-                  
-                  >
-                  <Menu.Item key='socialmedia1'>Twitter</Menu.Item>
-                  <Menu.Item key='socialmedia2'>Instagram</Menu.Item>
-                </Menu.ItemGroup>
-              </SubMenu>
-            </Menu>
-          </Sider>
+      <Row>
+        <Col xs={24} style={{ backgroundColor: '#000', color: '#fff', padding: 16}}>
+          <Avatar style={{ float: 'right', marginTop: 6 }} size="large" icon={<UserOutlined />} />
+          <Title level={3} style={{ color: '#fff', paddingTop: 10 }}>Eddie Madrigal</Title>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={24} sm={6}>
+          <Menu 
+            defaultSelectedKeys={['dashboard']}
+            mode='inline'
+          >
+            <Menu.Item key='dashboard'>
+              Dashboard
+            </Menu.Item>
+            <SubMenu 
+              title={
+                <span>Social Media Platform</span>
+              }>
+              <Menu.ItemGroup
+                key='socialmediagroup' 
+                
+                >
+                <Menu.Item key='socialmedia1'>Twitter</Menu.Item>
+                <Menu.Item key='socialmedia2'>Instagram</Menu.Item>
+              </Menu.ItemGroup>
+            </SubMenu>
+          </Menu>
+        </Col>
+        <Col xs={24} sm={18} style={{ backgroundColor: '#ccc'}}>
           <Content style={{ padding: '0 50px' }}>
             <Breadcrumb style={{ margin: '16px 0' }}>
               <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
             </Breadcrumb>
             <div style={{ background: '#fff', padding: 24, minHeight: 580 }}>Content</div>
           </Content>
-        </Layout>
-        <Footer style={{ backgroundColor: '#000', color: '#fff', textAlign: 'center' }}>React Ant Design. Created by Eddie Madrigal</Footer>
-      </Layout>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={24}>
+          <p style={{ 
+            backgroundColor: '#000'
+            , color: '#fff'
+            , padding: 16
+            , textAlign: 'center' }}>
+              React Ant Design. Created by Eddie Madrigal
+          </p>
+        </Col>
+      </Row>
     </div>
   );
 

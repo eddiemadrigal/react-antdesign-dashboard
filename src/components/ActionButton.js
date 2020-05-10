55b3d5679cc83670bdd80c2d1c78fb13fb07e09a
Eddie Madrigal <edmadrigal@yahoo.com>
2020-05-09 19:05:27 -0700

    add / remove post refactored

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -1,17 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PlusOutlined } from '@ant-design/icons';
 
 const ActionButton = props => {
 
-  const { list } = props;
+  const [formOpen, setFormOpen] = useState(false);
 
-  const buttonText = list ? "Add another list" : "Add another card";
+  const openForm = () => {
+    setFormOpen(true);
+  }
+
+  const closeForm = () => {
+    setFormOpen(false);
+  }
+
+  const renderAddButton = () => {
+    const { list } = props;
+
+    const buttonText = list ? 'Add another list' : 'Add another card';
+    const buttonTextOpacity = list ? 1 : 0.5;
+    const buttonTextColor = list ? 'white' : 'inherit';
+    const buttonTextBackground = list ? 'rgba(0,0,0,.15)' : 'inherit';
+
+    return (
+      <div
+        onClick={openForm}
+          style = {{
+            ...styles.addButton,
+            opacity: buttonTextOpacity,
+            color: buttonTextColor,
+            backgroundColor: buttonTextBackground
+          }}
+        >
+        <PlusOutlined /><span>{ buttonText }</span>
+      </div>
+    )
+  }
+
+  const styles = {
+    addButton: {
+      cursor: 'pointer',
+    }
+  }
+
+  const renderForm = () => {
+    return (
+      <div>
+        <p>Form here ...</p>
+        <button onClick={closeForm}>Close Form</button>
+      </div>
+    )
+  }
 
   return (
-    <div>
-      <PlusOutlined /><span>{ buttonText }</span>
-    </div>
+    formOpen ? renderForm() : renderAddButton()
   )
 }
+
+
 
 export default ActionButton;
